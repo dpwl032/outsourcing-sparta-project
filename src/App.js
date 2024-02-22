7b77a00d54a6b7d711cd2418f8af87db935fedc6
yeji Sung <dpwl0332@naver.com>
2024-02-22 11:06:40 +0900

    feat : 프로젝트 셋업 완료

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Router from './router/Router';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Router />
+    </>
+  );
 }
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>다들 팀프로젝트 화이팅해봐욤!</div>
+    </>
+  );
 }
 
 export default Home;
