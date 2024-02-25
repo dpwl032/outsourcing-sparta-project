7bcdcef2fe0b85a86e98f72a2f9e86856854a3ee
yeji Sung <dpwl0332@naver.com>
2024-02-26 01:26:52 +0900

    add: footer 구현 완료

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,60 @@
 import React from 'react';
-
+import styled from 'styled-components';
 const Footer = () => {
-  return <div></div>;
+  return (
+    <>
+      <FooterWrap>
+        <FooterInsideLogo>
+          <p>로고</p>
+        </FooterInsideLogo>
+        <FooterItem>
+          <FooterList>
+            <li>CONTACT</li>
+            <li>NOTICE</li>
+            <li>운영관리방침</li>
+            <li>개인정보처리방침</li>
+            <li>PARTNER SUPPORT</li>
+          </FooterList>
+        </FooterItem>
+        <CopyRight>
+          <p>COPYRIGHT 2024 (C) BY SPARTA 13 TEAM. OUTSOURCING TEAM PROJECT</p>
+        </CopyRight>
+      </FooterWrap>
+    </>
+  );
 };
 
 export default Footer;
+
+const FooterWrap = styled.div`
+  background-color: #f5f4f4d7;
+`;
+
+const FooterInsideLogo = styled.div`
+  height: 100px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CopyRight = styled.div`
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #666666;
+  border-top: 1px solid #9e9e9e;
+`;
+
+const FooterItem = styled.div`
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: cente;
+`;
+
+const FooterList = styled.ul`
+  display: flex;
+  width: 1000px;
+  justify-content: space-around;
+`;
--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '../Header';
 
 const Layout = () => {
   return (
     <>
-      <Header />
       <div>
         <h1>Common Layout</h1>
         <p>로그인 여부와 상관없이 접근 가능한 페이지입니다.</p>
