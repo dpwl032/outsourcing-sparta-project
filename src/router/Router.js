8f69418f278def189f316df2230d4b610edd34e5
bongjin-lee <you@example.com>
2024-02-26 14:07:12 +0900

    add : router 디테일 페이지 생성

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
+import Detail from '../pages/Detail';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path='/detail/:id' element={<Detail />} />
     </Routes>
   );
 };
