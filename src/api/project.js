3040e98ad6cc9a388b226140089fe8ebb084f59a
bongjin-lee <you@example.com>
2024-02-26 14:06:29 +0900

    update : 데이터 불러오기

--- src/api/project.js
+++ src/api/project.js
@@ -1,1 +1,16 @@
 import axios from 'axios';
+
+// export const detailapi = axios.create({
+//   baseURL: 'http://localhost:4000',
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// });
+
+const getTodos = async () => {
+    const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/class`)
+    console.log(response.data);
+    return response.data;
+}
+
+export { getTodos};
