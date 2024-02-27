ac2819aeb15a3e5ffbaaa3e12fac8024a9c044a9
김소혜 <nanasisan@naver.com>
2024-02-27 19:02:12 +0900

    🔥 del : 불필요 reducer 삭제

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,12 +1,10 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import auth from '../modules/authSlice';
-import businessInfoReducer from '../../redux/modules/businessInfoSlice.js';
 
 const rootReducer = combineReducers({ auth });
 
 const store = configureStore({
   reducer: rootReducer,
-  businessInfo: businessInfoReducer,
 });
 
 export default store;
