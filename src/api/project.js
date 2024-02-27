eaf7e7fbf0ff9fc82cfdbc55e7fe34994f0d9b58
bongjin-lee <you@example.com>
2024-02-27 11:01:47 +0900

    fix: 데이터 불러오기

--- src/api/project.js
+++ src/api/project.js
@@ -7,10 +7,10 @@ import axios from 'axios';
 //   }
 // });
 
-const getTodos = async () => {
+// 데이터만 불러오기
+export const getTodos = async () => {
     const response = await axios.get(`${process.env.REACT_APP_SERVER_URL}/class`)
     console.log(response.data);
     return response.data;
 }
 
-export { getTodos};
