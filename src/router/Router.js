ea24a5d6f0e258afe1560f7fb5b3f6fe9cea2438
김소혜 <nanasisan@naver.com>
2024-02-27 09:44:13 +0900

    ✨ update  : Route 추가

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,10 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/Home';
+import AddBusinessInfo from '../pages/RegisterBusinessInfo.jsx'
+import WriteReview from '../pages/WriteReview.jsx'
+import DetailInfoPage from '../pages/DetailInfoPage.jsx';
+import BusinessList from '../pages/BusinessList.jsx';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path='/RegisteringInfo' element={<AddBusinessInfo />} />
+      <Route path='/WritingReview' element={<WriteReview />} />
+      <Route path='/List' element={<BusinessList />} />
+      <Route path='/:id' element={<DetailInfoPage/>} />
     </Routes>
   );
 };
