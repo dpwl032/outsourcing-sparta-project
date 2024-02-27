b57fa65e1d35a096712783578676e71404fb0745
김소혜 <nanasisan@naver.com>
2024-02-27 09:49:25 +0900

    🎉 add : Register Page

---
+++ src/pages/BusinessList.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { db } from '.././firebase';
+import { collection, getDocs } from 'firebase/firestore';
+
+function BusinessList() {
+  const [businessInfos, setBusinessInfos] = useState([]);
+
+  useEffect(() => {
+    const fetchBusinessInfos = async () => {
+      const querySnapshot = await getDocs(collection(db, 'BusinessInfo'));
+      const businessList = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setBusinessInfos(businessList);
+    };
+
+    fetchBusinessInfos();
+  }, []);
+
+  return (
+    <ul>
+      {businessInfos.map((businessInfo) => (
+        <li key={businessInfo.id}>
+          <Link to={`/${businessInfo.id}`}>{businessInfo.title}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default BusinessList;
