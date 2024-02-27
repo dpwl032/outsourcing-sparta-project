47988e67f73c9221bf56aca4d4ad4f6cca456a79
김소혜 <nanasisan@naver.com>
2024-02-27 11:22:08 +0900

    ✨ update  : 프로그램 정보 리스트 출력

--- src/pages/auth/Home.jsx
+++ src/pages/auth/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import BusinessList from '../BusinessList'
 
 function Home() {
   return (
     <>
       <div>다들 팀프로젝트 화이팅해봐욤!</div>
+      <BusinessList />
       <Link to="/detail">API 테스트</Link>
     </>
   );
