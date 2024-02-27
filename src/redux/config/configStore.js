3b933a8420b9c7de672ac5417b9a1c5a71f5722d
김소혜 <nanasisan@naver.com>
2024-02-27 09:47:58 +0900

    ✨ update   : 업체정보 등록용 reducer 입력

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import businessInfoReducer from '../../api/businessInfoSlice.js';
 
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    businessInfo: businessInfoReducer,
+  }
 });
 
 export default store;
