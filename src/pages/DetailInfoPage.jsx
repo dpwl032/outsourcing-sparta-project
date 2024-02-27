bee3f9406accacff2e9c0e8fc8d7cccf802472c5
김소혜 <nanasisan@naver.com>
2024-02-27 21:03:49 +0900

    ✨ update  : 삭제 기능 구현

--- src/pages/DetailInfoPage.jsx
+++ src/pages/DetailInfoPage.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import WriteReview from "../components/WriteReview";
 import EditBusinessInfo from "../components/EditBusinessInfo";
 
 function DetailInfoPage() {
   const { id } = useParams();
+  const navigate = useNavigate();
   const [businessInfo, setBusinessInfo] = useState(null);
-  const [reviews, setReviews] = useState(null);
+  const [reviews, setReviews] = useState([]);
   const [userRole, setUserRole] = useState('');
   const [isEditing, setIsEditing] = useState(false);
 
@@ -23,7 +24,7 @@ function DetailInfoPage() {
 
     const fetchReviews = async () => {
       try {
-        const response = await axios.get(`http://localhost:5000/reviews`);
+        const response = await axios.get(`http://localhost:5000/reviews/${id}`);
         setReviews(response.data);
       } catch (error) {
         console.error("리뷰 정보를 가져오는 중 오류 발생:", error);
@@ -41,14 +42,22 @@ function DetailInfoPage() {
     setIsEditing(true);
   };
 
+  const handleDelete = async () => {
+    if (window.confirm("정말로 삭제하시겠습니까?")) {
+      try {
+        await axios.delete(`http://localhost:5000/businessInfo/${id}`);
+        navigate('/'); // Redirect to home or another appropriate page
+      } catch (error) {
+        console.error("업체 정보 삭제 중 오류 발생:", error);
+        alert('삭제 실패. 다시 시도해주세요.');
+      }
+    }
+  };
+
   const handleSaved = (updatedInfo) => {
     setBusinessInfo(updatedInfo);
     setIsEditing(false);
   };
-
-  if (isEditing) {
-    return <EditBusinessInfo businessInfo={businessInfo} onSaved={handleSaved} />;
-  }
 
   return (
     <div>
@@ -61,7 +70,7 @@ function DetailInfoPage() {
           {userRole === 'host' && (
             <>
               <button type="button" onClick={handleEdit}>수정</button>
-              <button type="button">삭제</button>
+              <button type="button" onClick={handleDelete}>삭제</button>
             </>
           )}
           {userRole !== 'host' && <WriteReview />}
@@ -76,7 +85,7 @@ function DetailInfoPage() {
         <EditBusinessInfo businessInfo={businessInfo} onSaved={handleSaved} />
       )}
     </div>
-  )
-};
+  );
+}
 
 export default DetailInfoPage;
