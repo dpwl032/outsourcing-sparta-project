ba0783209781a1c53ae4ef4fca456d311d12e1c6
김소혜 <nanasisan@naver.com>
2024-02-27 09:46:17 +0900

    🎉 add : firebase 기본 설정

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,18 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from 'firebase/storage';
+import { getAuth } from '@firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyB0u6sTT9x_yX4C6EKWEQcCGuKe3UvPX1I",
+  authDomain: "sparta-outsourcing-i3.firebaseapp.com",
+  projectId: "sparta-outsourcing-i3",
+  storageBucket: "sparta-outsourcing-i3.appspot.com",
+  messagingSenderId: "336955638558",
+  appId: "1:336955638558:web:83951f8c730bb777b93f0f"
 };
 
 const app = initializeApp(firebaseConfig);
-
-export default app;
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
